505e2133258ef00d87637ed708c915539c36e8f0
vikram <vikram@gryt.fit>
2020-10-27 15:45:37 +0530

    Fixed tcp_message

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,31 +4,61 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:flutter/services.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
 void main() {
-  runApp(VikramsApp());
+  runApp(DisplayApp());
 }
 
-class VikramsApp extends StatefulWidget {
+class DisplayApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return VikramsAppState();
+    return DisplayAppState();
   }
 }
 
-class VikramsAppState extends State<VikramsApp> {
-  IPAddressField ipAddressField;
+class DisplayAppState extends State<DisplayApp> {
+  TextField ipAddressField;
+  TextField portField;
+  TextField messageField;
+  final messageFieldController = TextEditingController();
+  final portFieldController = TextEditingController();
+  final ipAddressFieldController = TextEditingController();
+
+  @override
+  void dispose() {
+    messageFieldController.dispose();
+    portFieldController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
-    this.ipAddressField = IPAddressField();
+    this.ipAddressField = TextField(
+        decoration: new InputDecoration(labelText: "IP Address"),
+        keyboardType: TextInputType.number,
+        controller: ipAddressFieldController);
+    this.portField = new TextField(
+      decoration: new InputDecoration(labelText: "PORT"),
+      keyboardType: TextInputType.number,
+      inputFormatters: <TextInputFormatter>[
+        FilteringTextInputFormatter.digitsOnly
+      ],
+      controller: portFieldController,
+    );
+    this.messageField = new TextField(
+        decoration: InputDecoration(labelText: "Enter your message to display"),
+        controller: messageFieldController);
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: Text("Vikrams App"),
+              title: Text("LED Display controller"),
             ),
             body: Column(
               children: [
                 ipAddressField,
-                Text("Display Address"),
-                Text("Message"),
+                portField,
+                messageField,
                 RaisedButton(
                     child: Text('Send to device'),
                     onPressed: sendToDisplayDevice)
@@ -37,9 +67,16 @@ class VikramsAppState extends State<VikramsApp> {
   }
 
   void sendToDisplayDevice() {
-    print("Send to Device" + ipAddressField.ipAddress);
+    print("Send to Device" +
+        ipAddressFieldController.text +
+        " " +
+        messageFieldController.text +
+        " " +
+        portFieldController.text);
 
-    Socket.connect("192.168.1.16", 5005).then((socket) {
+    Socket.connect(
+            ipAddressFieldController.text, int.parse(portFieldController.text))
+        .then((socket) {
       print("Sending message");
       socket.write("Testmessage");
       socket.destroy();
